9dd80ffb2db2bba9903865e51030541ce48c4fe2
SO\81809 <okumurasota@gmail.com>
2023-09-21 14:16:47 +0900

    デバッグstr

--- docs/main.js
+++ docs/main.js
@@ -9,9 +9,9 @@ function init() {
     // 簡易的なOS判定
     os = detectOSSimply();
     if (os == "iphone") {
-        alert("iphone");
+        // alert("iphone");
         // safari用。DeviceOrientation APIの使用をユーザに許可して貰う
-        document.getElementById("permit").addEventListener("click", permitDeviceOrientationForSafari);
+        // document.getElementById("permit").addEventListener("click", permitDeviceOrientationForSafari);
 
         window.addEventListener(
             "deviceorientation",
@@ -19,7 +19,7 @@ function init() {
             true
         );
     } else if (os == "android") {
-        alert("android");
+        // alert("android");
         window.addEventListener(
             "deviceorientationabsolute",
             orientation,
@@ -52,24 +52,27 @@ function detectOSSimply() {
 }
 
 // iPhone + Safariの場合はDeviceOrientation APIの使用許可をユーザに求める
-function permitDeviceOrientationForSafari() {
-    alert("request");
-    DeviceOrientationEvent.requestPermission()
-        .then(response => {
-            if (response === "granted") {
-                window.addEventListener(
-                    "deviceorientation",
-                    detectDirection
-                );
-            }
-        })
-        .catch(console.error);
-}
+// function permitDeviceOrientationForSafari() {
+//     alert("request");
+//     DeviceOrientationEvent.requestPermission()
+//         .then(response => {
+//             if (response === "granted") {
+//                 window.addEventListener(
+//                     "deviceorientation",
+//                     detectDirection
+//                 );
+//             }
+//         })
+//         .catch(console.error);
+// }
 
 var aX = 0, aY = 0, aZ = 0;      // 加速度の値を入れる変数を3個用意
 
-let flag = false;
-alert("更新9");
+let flag = 0;
+alert("更新10");
+
+let str = "";
+let cnt = 0;
 
 // 加速度センサの値が変化したら実行される devicemotion イベント
 window.addEventListener("devicemotion", (dat) => {
@@ -79,20 +82,26 @@ window.addEventListener("devicemotion", (dat) => {
 
     let X=Math.abs(aX);
     let Z=Math.abs(aZ);
-    alert(`加速度の検知${aX},${aY},${aZ},${X},${Z}`);
+    str += `[${X},${Z}],`;
+    cnt ++;
+    if(cnt == 20){
+        alert(str);
+        cnt = 0;
+    }
 
-    if(X>=2 || Z>=2) flag = true;
+    if((X>=2 || Z>=2) && flag == 0) flag = 1;
 
 
-    if(flag){
+    if(flag == 1){
+        flag *= -1; //flag 1->-1
         alert("検知");
-        // document.body.insertAdjacentHTML("beforeend",
-        //     `<a-scene>
-        //         <a-entity camera look-controls orbit-controls="target: 0 1.6 -0.5; maxPolarAngle:180; minDistance: 0.5; maxDistance: 200; initialPosition: 0 0 30"></a-entity>
-        //         <a-gltf-model  id="test" src="./model/map.glb"  position="0 -1 0" rotation="0 0 0" scale="8 8 8"></a-gltf-model>
-        //         <a-sky color="#9EA1E7"></a-sky>
-        //     </a-scene>`
-        // )
+        document.body.insertAdjacentHTML("beforeend",
+            `<a-scene>
+                <a-entity camera look-controls orbit-controls="target: 0 1.6 -0.5; maxPolarAngle:180; minDistance: 0.5; maxDistance: 200; initialPosition: 0 0 30"></a-entity>
+                <a-gltf-model  id="test" src="./model/map.glb"  position="0 -1 0" rotation="0 0 0" scale="8 8 8"></a-gltf-model>
+                <a-sky color="#9EA1E7"></a-sky>
+            </a-scene>`
+        )
     };
 
 });
